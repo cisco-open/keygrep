10d3b22731b616877020b349bbd8c51b49c69c28
Adam Reiser <reiser@defensivecomputing.io>
2025-06-17 23:42:32 -0700

    Fix test key download script, improve docstrings, fix indenting

--- tests/test_keygrep.py
+++ tests/test_keygrep.py
@@ -10,7 +10,7 @@ from keygrep.keygrep_utility import get_privkey_data
 # Use download-test-keys.sh to set up
 
 def run_keygrep(in_keys_dir, write_results_dir, include_mangled):
-    """Run keygrep"""
+    """Run keygrep and write results to the provided directory."""
     kc = KeyChain(output_dir=write_results_dir, path_prefix="", include_mangled=include_mangled)
     kc.load_private_keys(in_keys_dir)
     kc.load_public_keys(in_keys_dir)
@@ -19,7 +19,8 @@ def run_keygrep(in_keys_dir, write_results_dir, include_mangled):
     kc.write_public_keys()
 
 def test_identical_keys(tmp_path):
-    """Check that public/private keys with the same fingerprint are treated as the same."""
+    """Check that for both public and private keys, keys with the same
+    fingerprint are considered the same key."""
     out_dir = tmp_path / "findings"
     test_data_dir = Path(__file__).parent / "test-keys/identical-keys"
 
@@ -27,9 +28,9 @@ def test_identical_keys(tmp_path):
 
     with open(out_dir / "private.json", "r", encoding="utf-8") as inf:
         priv_data = json.load(inf)
-        assert len(priv_data) == 1
-        assert len(priv_data[0]["privkey_locations"]) == 5
-        print("Identical private keys are correctly collapsed")
+    assert len(priv_data) == 1
+    assert len(priv_data[0]["privkey_locations"]) == 5
+    print("Identical private keys are correctly collapsed")
 
 def test_invalid_public_keys(tmp_path):
     """Check that distinct invalid public keys are tracked."""
@@ -40,8 +41,8 @@ def test_invalid_public_keys(tmp_path):
 
     with open(out_dir / "public.json", "r", encoding="utf-8") as inf:
         pub_data = json.load(inf)
-        assert len(pub_data) == 2
-        print("Distinct invalid public keys are separately recorded")
+    assert len(pub_data) == 2
+    print("Distinct invalid public keys are separately recorded")
 
 def test_openssh_keys(tmp_path):
     """Test that the expected results are generated from OpenSSH test keys."""
@@ -70,7 +71,7 @@ def test_openssh_keys(tmp_path):
         if "ENCRYPTED" not in key["priv"]:
             assert key["pub"] is not None
             assert key["sha256"] is not None
-        # For PEM/PKCS8 formatted private keys, we cannot obtain the fingerprint or public key without the passphrase
+        # For PEM/PKCS8 formatted encrypted private keys, we cannot obtain the fingerprint or public key without the passphrase
         else:
             assert key["pub"] is None
             assert key["sha256"] is None
@@ -95,18 +96,19 @@ def test_openssh_keys(tmp_path):
 
     assert n == 22
 
-    # if include_mangled is False, we should always have a public key string
+    # If include_mangled is False, we should always have a public key string
     # and fingerprint for each public key
     for key in pub_data:
         assert key["pub"] is not None
         assert key["sha256"] is not None
 
 def test_encrypted_and_clear(tmp_path):
-    """Check that cleartext keys replace encrypted keys if we have copies of both.behavior if we have both an encrypted and a cleartext copy of a key."""
+    """Check that cleartext keys replace encrypted keys if we have copies of
+    both."""
     out_dir = tmp_path / "findings"
 
     kc = KeyChain(output_dir=out_dir, path_prefix="", include_mangled=False)
-    # Load encrypted first, in a format where we can obtain the fingerprint.
+    # Load encrypted first, in a format where we can obtain the fingerprint
     kc.load_private_keys(Path(__file__).parent / "test-keys/openssh/ed25519_1_pw")
     # Load a cleartext copy of the same key
     kc.load_private_keys(Path(__file__).parent / "test-keys/openssh/ed25519_1")
